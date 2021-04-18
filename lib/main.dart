f87b28038b3fab2d51467815cc1756345c092e3e
maximzec <maximzec3200@gmail.com>
2021-04-19 00:48:01 +0300

    finished header style and texts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,33 +13,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.blue,
         ),
-        home: Column(
-          children: [
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Container(
-                height: 100,
-                decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                  )
-                ]),
-              ),
-            )
-          ],
+        home: Scaffold(
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FractionallySizedBox(
+                widthFactor: 1,
+                child: Container(
+                  height: 100,
+                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                    )
+                  ]),
+                  child: Row(
+                    children: [
+                      Padding(
+                        child: Text(
+                          'Блог Максима Фроленко',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 24,
+                          ),
+                        ),
+                        padding: EdgeInsets.only(left: 12),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ));
   }
 }

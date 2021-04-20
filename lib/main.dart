b3cec8e04e244764098edf16122af56cb86c8026
maximzec <maximzec3200@gmail.com>
2021-04-21 02:04:49 +0300

    finished header again :)

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,11 @@ void main() {
   runApp(MyApp());
 }
 
+Color daintree = const Color(0xFF012731);
+Color redSalsa = const Color(0xFFFD3A4A);
+
+TextStyle appBarActionsStyle = new TextStyle(color: Colors.white, fontSize: 20);
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -13,34 +18,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          fontFamily: 'Product Sans',
         ),
         home: Scaffold(
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              FractionallySizedBox(
-                widthFactor: 1,
-                child: Container(
-                  height: 100,
-                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                    )
-                  ]),
+          appBar: AppBar(
+            backgroundColor: daintree,
+            title: Padding(
+              child: const Text("Блог Максима Фроленко"),
+              padding: EdgeInsets.only(left: 120),
+            ),
+            toolbarHeight: 75,
+            actions: [
+              Padding(
+                padding: EdgeInsets.only(right: 120),
+                child: Align(
+                  alignment: Alignment.center,
                   child: Row(
                     children: [
                       Padding(
+                        padding: EdgeInsets.only(right: 60),
                         child: Text(
-                          'Блог Максима Фроленко',
-                          style: TextStyle(
-                            fontFamily: 'Roboto',
-                            fontSize: 24,
-                          ),
+                          "Портфолио",
+                          style: appBarActionsStyle,
                         ),
-                        padding: EdgeInsets.only(left: 12),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 60),
+                        child: Text("Статьи", style: appBarActionsStyle),
+                      ),
+                      Text(
+                        "Связь",
+                        style: appBarActionsStyle,
                       )
                     ],
                   ),
